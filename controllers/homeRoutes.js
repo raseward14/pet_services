c7ee2eccd147626d47d8cdf988d7f968f0efa311
Richard Seward <raseward14@gmail.com>
2021-04-07 20:47:29 -0600

    homeRoute created

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Account, Appointment, Employee } = require('../models');
+const withAuth = require('../utils/auth');
+
+// renders the homepage
+router.get('/', async (req, res) => {
+    res.render
+});
+
+// renders your profile, use withAuth middleware to prevent access
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        // find logged in user based on the session ID
+        const accountData = await Account.findByPk(req.session.user_id, {
+            include: [{ model: Appointment  }]
+        })
+
+        // serialize data for template
+        const account = accountData.get({ plain: true });
+
+        // render profile handlebars
+        res.render('profile', {
+            ...account,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// renders login page
+router.get('/login', (req, res) => {
+    // if user is already logged in, redirect req to another route
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
+
