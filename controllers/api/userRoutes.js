63caa30475abaf409b24c736ff067a9c679481be
Richard Seward <raseward14@gmail.com>
2021-04-10 10:54:14 -0600

    added userRoute login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { User } = require('../../models')
+const { Model } = require('sequelize/types');
+const { Account } = require('../../models')
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await Account.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
@@ -32,3 +33,16 @@ router.post('/login', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+router.post('/logout', async (req, res) => {
+  if (req.session.logged_in) {
+    await req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    console.log('user');
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
