bc950aa3947f024a620fc810020eb1ef125e1b53
Brandon Laptop <brandon.minard@gmail.com>
2021-04-10 13:06:59 -0600

    example for testingExports

--- utils/jsHelp.js
+++ utils/jsHelp.js
@@ -86,3 +86,8 @@ async function scheduleAppt(accountIdParam, employeeIdParam, dateParam, timeslot
 // }
 
 // console.log(dbScheduleToObj("2021-4-10/2021-4-11;0;1;2;3;4;5;6/2021-4-12;0;1;2;3;4;5;6/2021-4-13;0;1;2;3;4;5;6/2021-4-14;0;1;2;3;4;5;6/2021-4-15;0;1;2;3;4;5;6/2021-4-16"))
+module.exports = {
+    dbScheduleToObj: dbScheduleToObj,
+    dbObjToString: dbObjToString,
+    scheduleAppt: scheduleAppt
+};
