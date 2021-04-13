88847f5cbbe689fc2266b24f5f561ff42ecb4e40
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-04-13 12:04:35 -0600

    working on api pull for calendar

--- controllers/api/schedule.js
+++ controllers/api/schedule.js
@@ -3,7 +3,7 @@ const helpers = require('../../utils/jsHelp.js')
 const sequelize = require('../../config/connection.js');
 const { Employee } = require('../../models')
 
-router.get('/', async (req, res) => {
+router.get('/dates', async (req, res) => {
   try {
     const employees = await Employee.findAll();
 
@@ -12,6 +12,10 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+})
+
+router.get('/dates', (req, res) => {
+
 })
 
 module.exports = router;
