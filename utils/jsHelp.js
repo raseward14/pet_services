26378a73c35d5b7fa021b842f595201a4af4e60a
Brandon Laptop <brandon.minard@gmail.com>
2021-04-14 20:49:28 -0600

    added a simplify function, hooked it into schedule

--- utils/jsHelp.js
+++ utils/jsHelp.js
@@ -49,8 +49,8 @@ function dbObjToString(dbObj) {
 //And then makes a new appointment, and removes the timeslot from the schedule obj.
 async function scheduleAppt(accountIdParam, employeeIdParam, dateParam, timeslotParam, scheduleObj) {
 
-    await sequelize.sync({ force: true })((err) => res.json(err));;
-    works
+    await sequelize.sync({ force: true })((err) => res.json(err));
+    //works
     const appt = await Appointment.create({
         employeeId: employeeIdParam,
         accountId: accountIdParam,
@@ -71,6 +71,31 @@ async function scheduleAppt(accountIdParam, employeeIdParam, dateParam, timeslot
         }
     ).catch((err) => console.log(err))
 }
+
+// async function scheduelGoodAppt(userAccIdParam, emplIdParam, dateParam, milTimeSlot){
+//     await sequelize.sync({ force: true })((err) => res.json(err));
+//     const appt = await Appointment.create({
+//         employeeId: emplIdParam,
+//         accountId: userAccIdParam,
+//         timeSlot: milTimeSlot,
+//         date: dateParam
+//     }).catch((err) => console.log(err));
+// }
+
+function simplifyTimeSlots(bigAssEmployeeThing) {
+    let returnObj = {}
+    for (let index = 0; index < bigAssEmployeeThing.length; index++) {
+        const element = bigAssEmployeeThing[index];
+        if (element.milTimeSlots) {
+            returnObj[element.date] = element.milTimeSlots.split(",")
+        } else {
+            returnObj[element.date] = null
+        }
+    }
+    return returnObj
+}
+
+
 
 //pass in all weeks to this as an array to get back a more condensed version of itself
 function condenseScheduleObjWeeks(ArrOfScheduleObj) {
@@ -146,61 +171,92 @@ function makeTimesIntoTimeSlots(timeslots, start, breakTime) {
     }
 }
 
+let testingStuff = [
+    {
+        "id": 1,
+        "accountId": 1,
+        "employeeId": 1,
+        "date": "2021-3-14",
+        "milTimeSlots": "9,10,12,13,14,15,16"
+    },
+    {
+        "id": 2,
+        "accountId": 1,
+        "employeeId": 1,
+        "date": "2021-3-15",
+        "milTimeSlots": "9,10,12,13,14,15,16"
+    },
+    {
+        "id": 3,
+        "accountId": 1,
+        "employeeId": 1,
+        "date": "2021-3-16",
+        "milTimeSlots": "9,10,12,13,14,15,16"
+    },
+    {
+        "id": 4,
+        "accountId": 1,
+        "employeeId": 1,
+        "date": "2021-3-17",
+        "milTimeSlots": null
+    }
+]
+// console.log(simplifyTimeSlots(testingStuff))
 
 // let thing = condenseScheduleObjWeeks(
 
 // )
 // console.log(thing)
 
-const stuff = [
-    {
-        id: 1,
-        name: 'Johnson',
-        job: 'dog walker',
-        slotHrs: 1,
-        shiftStart: 9,
-        shiftEnd: 17,
-        timeSlots: [],
-        totalSlots: 7,
-        accountid: 1,
-        week1: '2021-3-12;0;1;2;3;4;5;6/2021-3-13;0;1;2;3;4;5;6/2021-3-14;0;1;2;3;4;5;6/2021-3-15;0;1;2;3;4;5;6/2021-3-16/2021-3-17/2021-3-18;0;1;2;3;4;5;6',
-        week2: '2021-3-19;0;1;2;3;4;5;6/2021-3-20;0;1;2;3;4;5;6/2021-3-21;0;1;2;3;4;5;6/2021-3-22;0;1;2;3;4;5;6/2021-3-23/2021-3-24/2021-3-25;0;1;2;3;4;5;6',
-        week3: '2021-3-26;0;1;2;3;4;5;6/2021-3-27;0;1;2;3;4;5;6/2021-3-28;0;1;2;3;4;5;6/2021-3-29;0;1;2;3;4;5;6/2021-3-30/2021-4-1/2021-4-2;0;1;2;3;4;5;6',
-        week4: '2021-4-3;0;1;2;3;4;5;6/2021-4-4;0;1;2;3;4;5;6/2021-4-5;0;1;2;3;4;5;6/2021-4-6;0;1;2;3;4;5;6/2021-4-7/2021-4-8/2021-4-9;0;1;2;3;4;5;6'
-    },
-    {
-        id: 2,
-        name: 'Johnny',
-        job: 'dog walker',
-        slotHrs: 1,
-        shiftStart: 9,
-        shiftEnd: 17,
-        timeSlots: [],
-        totalSlots: 7,
-        accountid: 2,
-        week1: '2021-3-12/2021-3-13/2021-3-14;0;1;2;3;4;5;6/2021-3-15;0;1;2;3;4;5;6/2021-3-16;0;1;2;3;4;5;6/2021-3-17;0;1;2;3;4;5;6/2021-3-18',
-        week2: '2021-3-19/2021-3-20/2021-3-21;0;1;2;3;4;5;6/2021-3-22;0;1;2;3;4;5;6/2021-3-23;0;1;2;3;4;5;6/2021-3-24;0;1;2;3;4;5;6/2021-3-25',
-        week3: '2021-3-26/2021-3-27/2021-3-28;0;1;2;3;4;5;6/2021-3-29;0;1;2;3;4;5;6/2021-3-30;0;1;2;3;4;5;6/2021-4-1;0;1;2;3;4;5;6/2021-4-2',
-        week4: '2021-4-3/2021-4-4/2021-4-5;0;1;2;3;4;5;6/2021-4-6;0;1;2;3;4;5;6/2021-4-7;0;1;2;3;4;5;6/2021-4-8;0;1;2;3;4;5;6/2021-4-9'
-    },
-    {
-        id: 3,
-        name: 'Kenneth',
-        job: 'dog walker',
-        slotHrs: 1,
-        shiftStart: 9,
-        shiftEnd: 17,
-        timeSlots: [],
-        totalSlots: 7,
-        accountid: 3,
-        week1: '2021-3-12/2021-3-13;0;1;2;3;4;5;6/2021-3-14/2021-3-15;0;1;2;3;4;5;6/2021-3-16;0;1;2;3;4;5;6/2021-3-17;0;1;2;3;4;5;6/2021-3-18;0;1;2;3;4;5;6',
-        week2: '2021-3-19/2021-3-20;0;1;2;3;4;5;6/2021-3-21/2021-3-22;0;1;2;3;4;5;6/2021-3-23;0;1;2;3;4;5;6/2021-3-24;0;1;2;3;4;5;6/2021-3-25;0;1;2;3;4;5;6',
-        week3: '2021-3-26/2021-3-27;0;1;2;3;4;5;6/2021-3-28/2021-3-29;0;1;2;3;4;5;6/2021-3-30;0;1;2;3;4;5;6/2021-4-1;0;1;2;3;4;5;6/2021-4-2;0;1;2;3;4;5;6',
-        week4: '2021-4-3/2021-4-4;0;1;2;3;4;5;6/2021-4-5/2021-4-6;0;1;2;3;4;5;6/2021-4-7;0;1;2;3;4;5;6/2021-4-8;0;1;2;3;4;5;6/2021-4-9;0;1;2;3;4;5;6'
-    }
-]
+// const stuff = [
+//     {
+//         id: 1,
+//         name: 'Johnson',
+//         job: 'dog walker',
+//         slotHrs: 1,
+//         shiftStart: 9,
+//         shiftEnd: 17,
+//         timeSlots: [],
+//         totalSlots: 7,
+//         accountid: 1,
+//         week1: '2021-3-12;0;1;2;3;4;5;6/2021-3-13;0;1;2;3;4;5;6/2021-3-14;0;1;2;3;4;5;6/2021-3-15;0;1;2;3;4;5;6/2021-3-16/2021-3-17/2021-3-18;0;1;2;3;4;5;6',
+//         week2: '2021-3-19;0;1;2;3;4;5;6/2021-3-20;0;1;2;3;4;5;6/2021-3-21;0;1;2;3;4;5;6/2021-3-22;0;1;2;3;4;5;6/2021-3-23/2021-3-24/2021-3-25;0;1;2;3;4;5;6',
+//         week3: '2021-3-26;0;1;2;3;4;5;6/2021-3-27;0;1;2;3;4;5;6/2021-3-28;0;1;2;3;4;5;6/2021-3-29;0;1;2;3;4;5;6/2021-3-30/2021-4-1/2021-4-2;0;1;2;3;4;5;6',
+//         week4: '2021-4-3;0;1;2;3;4;5;6/2021-4-4;0;1;2;3;4;5;6/2021-4-5;0;1;2;3;4;5;6/2021-4-6;0;1;2;3;4;5;6/2021-4-7/2021-4-8/2021-4-9;0;1;2;3;4;5;6'
+//     },
+//     {
+//         id: 2,
+//         name: 'Johnny',
+//         job: 'dog walker',
+//         slotHrs: 1,
+//         shiftStart: 9,
+//         shiftEnd: 17,
+//         timeSlots: [],
+//         totalSlots: 7,
+//         accountid: 2,
+//         week1: '2021-3-12/2021-3-13/2021-3-14;0;1;2;3;4;5;6/2021-3-15;0;1;2;3;4;5;6/2021-3-16;0;1;2;3;4;5;6/2021-3-17;0;1;2;3;4;5;6/2021-3-18',
+//         week2: '2021-3-19/2021-3-20/2021-3-21;0;1;2;3;4;5;6/2021-3-22;0;1;2;3;4;5;6/2021-3-23;0;1;2;3;4;5;6/2021-3-24;0;1;2;3;4;5;6/2021-3-25',
+//         week3: '2021-3-26/2021-3-27/2021-3-28;0;1;2;3;4;5;6/2021-3-29;0;1;2;3;4;5;6/2021-3-30;0;1;2;3;4;5;6/2021-4-1;0;1;2;3;4;5;6/2021-4-2',
+//         week4: '2021-4-3/2021-4-4/2021-4-5;0;1;2;3;4;5;6/2021-4-6;0;1;2;3;4;5;6/2021-4-7;0;1;2;3;4;5;6/2021-4-8;0;1;2;3;4;5;6/2021-4-9'
+//     },
+//     {
+//         id: 3,
+//         name: 'Kenneth',
+//         job: 'dog walker',
+//         slotHrs: 1,
+//         shiftStart: 9,
+//         shiftEnd: 17,
+//         timeSlots: [],
+//         totalSlots: 7,
+//         accountid: 3,
+//         week1: '2021-3-12/2021-3-13;0;1;2;3;4;5;6/2021-3-14/2021-3-15;0;1;2;3;4;5;6/2021-3-16;0;1;2;3;4;5;6/2021-3-17;0;1;2;3;4;5;6/2021-3-18;0;1;2;3;4;5;6',
+//         week2: '2021-3-19/2021-3-20;0;1;2;3;4;5;6/2021-3-21/2021-3-22;0;1;2;3;4;5;6/2021-3-23;0;1;2;3;4;5;6/2021-3-24;0;1;2;3;4;5;6/2021-3-25;0;1;2;3;4;5;6',
+//         week3: '2021-3-26/2021-3-27;0;1;2;3;4;5;6/2021-3-28/2021-3-29;0;1;2;3;4;5;6/2021-3-30;0;1;2;3;4;5;6/2021-4-1;0;1;2;3;4;5;6/2021-4-2;0;1;2;3;4;5;6',
+//         week4: '2021-4-3/2021-4-4;0;1;2;3;4;5;6/2021-4-5/2021-4-6;0;1;2;3;4;5;6/2021-4-7;0;1;2;3;4;5;6/2021-4-8;0;1;2;3;4;5;6/2021-4-9;0;1;2;3;4;5;6'
+//     }
+// ]
 
-console.log(getNameAndTimeSlotsForDay("2021-3-14", stuff))
+// console.log(getNameAndTimeSlotsForDay("2021-3-14", stuff))
 // condenseScheduleObjWeeks(
 //     ["2021-4-10/2021-4-11;0;1;2;3;4;5;6/2021-4-12;0;1;2;3;4;5;6/2021-4-13;0;1;2;3;4;5;6/2021-4-14;0;1;2;3;4;5;6/2021-4-15;0;1;2;3;4;5;6/2021-4-16",
 //         "2021-4-17/2021-4-18;0;1;2;3;4;5;6/2021-4-19;0;1;2;3;4;5;6/2021-4-20;0;1;2;3;4;5;6/2021-4-21;0;1;2;3;4;5;6/2021-4-22;0;1;2;3;4;5;6/2021-4-23",
@@ -225,5 +281,6 @@ console.log(getNameAndTimeSlotsForDay("2021-3-14", stuff))
 module.exports = {
     dbScheduleToObj: dbScheduleToObj,
     dbObjToString: dbObjToString,
-    scheduleAppt: scheduleAppt
+    scheduleAppt: scheduleAppt,
+    simplifyTimeSlots: simplifyTimeSlots
 };
