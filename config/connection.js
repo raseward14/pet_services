5ea12afc7928c16c792bdcf8794d4dc84db1f060
Richard Seward <raseward14@gmail.com>
2021-04-17 14:06:13 -0600

    connection.js file edited

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,17 @@
-require('dotenv').config({ path: __dirname + '/.env' });
-console.log(require('dotenv').config())
 const Sequelize = require('sequelize');
-
-
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    console.log(process.env.DB_NAME)
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
+require('dotenv').config();
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
     );
-}
-
 module.exports = sequelize;
